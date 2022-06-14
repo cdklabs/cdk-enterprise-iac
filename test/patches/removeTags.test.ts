04ff16d6150190f0d0f7e4eaf8585a4a1d6d4acc
Taylor Ondrey <ondreyt@amazon.com>
2022-06-14 10:28:31 -0400

    Add test for non 'Tags' property

--- test/patches/removeTags.test.ts
+++ test/patches/removeTags.test.ts
@@ -1,5 +1,6 @@
 import { Aspects, Stack } from 'aws-cdk-lib';
 import { Match, Template } from 'aws-cdk-lib/assertions';
+import { CfnBackupPlan } from 'aws-cdk-lib/aws-backup';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { RemoveTags } from '../../src/patches/removeTags';
 
@@ -19,9 +20,33 @@ describe('Remove Tags from resources', () => {
     }));
 
     const template = Template.fromStack(stack);
-    console.log(template);
     template.hasResourceProperties('AWS::EC2::EIP', {
       Tags: Match.absent(),
+    });
+  });
+
+  test('Remove tags with different name than "Tags"', () => {
+
+    new CfnBackupPlan(stack, 'TestBackupplan', {
+      backupPlan: {
+        backupPlanName: 'testBackupPlan',
+        backupPlanRule: [{
+          ruleName: 'SomeRule',
+          targetBackupVault: 'SomeBackupVault',
+        }],
+      },
+      backupPlanTags: {
+        SomeKey: 'SomeValue',
+      },
+    });
+    Aspects.of(stack).add(new RemoveTags({
+      cloudformationResource: 'AWS::Backup::BackupPlan',
+      tagPropertyName: 'BackupPlanTags',
+    }));
+
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties('AWS::Backup::BackupPlan', {
+      BackupPlanTags: Match.absent(),
     });
   });
 });
