1049361bb8474dc900a2e4c50d8d3b4834dfe630
Blake Barnett <blakbar@amazon.com>
2022-11-30 08:35:18 -0700

    removing cdk.out

--- cdk.out/asset.a3d8f99a1fc57241aa86a09cd0ad6d2fc916fc4353d654214c1f6f3b4ebbf551/config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
-echo $REGION
-sed -i  "s/REPLACE_REGION/$REGION/" /opt/aws/ansible-playbook-proxy/group_vars/all.yml
-sed -i  "s/REPLACE_REGION/$REGION/" /opt/aws/ansible-playbook-proxy/roles/ansible-role-cloudwatch/defaults/main.yaml
-sed -i  "s/REPLACE_REGION/$REGION/" /opt/aws/ansible-playbook-proxy/roles/ansible-role-squid/defaults/main.yaml
