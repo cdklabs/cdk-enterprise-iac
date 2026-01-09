ce0dd5e04aa87e1ce57e65d1bdb090382f11ec93
Zachary Prado <zpprado@amazon.com>
2026-01-09 10:01:05 -0800

    move compute hash logic to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,19 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
+import { createHash } from 'crypto';
+
 export const getResourceId = (resourcePath: string): string => {
   return resourcePath.replace(/[/:]/g, '-');
+};
+
+export const computeUniqueHash = (
+  nodePath: string,
+  region: string,
+  length: number = 8
+): string => {
+  const hash = createHash('shake256');
+  hash.update(`${nodePath}-${region}`);
+  const hashValue = hash.digest('hex');
+  return hashValue.substring(hashValue.length - length, hashValue.length);
 };
--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -2,7 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-import { getResourceId } from '../../src/utils/utils';
+import { getResourceId, computeUniqueHash } from '../../src/utils/utils';
 
 describe('Testing utils', () => {
   test('getResourceId returns valid name', () => {
@@ -14,5 +14,41 @@ describe('Testing utils', () => {
     expect(getResourceId(nodePath)).toBe(
       'Default-TestInlinePolicy-Resource-extra-stuff'
     );
+  });
+  describe('computeUniqueHash', () => {
+    test('is deterministic with same inputs', () => {
+      const nodePath = 'TestStack/TestRole';
+      const region = 'us-east-1';
+
+      const hash1 = computeUniqueHash(nodePath, region);
+      const hash2 = computeUniqueHash(nodePath, region);
+      const hash3 = computeUniqueHash(nodePath, region);
+
+      expect(hash1).toEqual(hash2);
+      expect(hash2).toEqual(hash3);
+    });
+
+    test('produces different hashes for different paths', () => {
+      const region = 'us-east-1';
+
+      const hash1 = computeUniqueHash('TestStack/RoleA', region);
+      const hash2 = computeUniqueHash('TestStack/RoleB', region);
+
+      expect(hash1).not.toEqual(hash2);
+    });
+
+    test('produces different hashes for different regions', () => {
+      const nodePath = 'TestStack/TestRole';
+
+      const hash1 = computeUniqueHash(nodePath, 'us-east-1');
+      const hash2 = computeUniqueHash(nodePath, 'us-west-2');
+
+      expect(hash1).not.toEqual(hash2);
+    });
+
+    test('returns 8 characters by default', () => {
+      const hash = computeUniqueHash('TestStack/TestRole', 'us-east-1');
+      expect(hash.length).toBe(8);
+    });
   });
 });
